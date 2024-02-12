aa462e4cfc50a8561c075c48a07c952e97b645d6
lucas <lucaston2323@gmail.com>
2024-02-12 20:23:35 -0300

    feat: logo icon and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import logo from './assets/books-icon.png';
+
 export function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className="mx-auto max-w-6xl my-12">
+            <img src={ logo } alt="books logo" className='max-w-20' />
+            
+            <form className="w-full">
+            <input 
+                type="text" 
+                placeholder='Busque em suas notas. . .'
+                className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+            />
+            </form>
+        </div>
     )
 }
 
