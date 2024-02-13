025ecb8d9af11cecca0a23a7fca0fc0bd3f07abd
lucas <lucaston2323@gmail.com>
2024-02-13 01:32:02 -0300

    feat: focus visible to notes cards

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
     return (
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <button className='rounded-md bg-slate-800 p-5 text-left space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none'>
             <span className='text-sm font-medium text-slate-300'>
                 há 2 dias
             </span>
@@ -9,6 +9,6 @@ export function NoteCard() {
             </p>
 
             <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
-        </div>
+        </button>
     )
 }
