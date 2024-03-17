47572eae449d221e700b5b55c264d040656f6fd5
lucas <lucaston2323@gmail.com>
2024-03-17 18:04:00 -0300

    feat: span and button to modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,4 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 interface NoteCardProps {
     note: {
@@ -24,8 +26,19 @@ export function NoteCard({note}: NoteCardProps) {
 
             <Dialog.Portal>
                 <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
-                <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none">
-                    oi
+                <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+                    <div className="flex flex-1 flex-col gap-3 p-5">
+                        <span className='text-sm font-medium text-slate-300'>
+                            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+                        </span>
+                        <p className='text-sm leading-6 text-slate-400'>
+                            {note.content}
+                        </p>
+                    </div>
+
+                    <button type="button" className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none">
+                        Deseja apagar essa nota?
+                    </button>
                 </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
