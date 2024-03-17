accde1418dd300439f6a276271518aaacadeb03f
lucas <lucaston2323@gmail.com>
2024-03-17 18:14:16 -0300

    feat: button close

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { X } from "lucide-react";
 
 interface NoteCardProps {
     note: {
@@ -27,6 +28,10 @@ export function NoteCard({note}: NoteCardProps) {
             <Dialog.Portal>
                 <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
                 <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+                    <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                        <X className="size-5"/>
+                    </Dialog.Close>
+
                     <div className="flex flex-1 flex-col gap-3 p-5">
                         <span className='text-sm font-medium text-slate-300'>
                             {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
@@ -36,8 +41,8 @@ export function NoteCard({note}: NoteCardProps) {
                         </p>
                     </div>
 
-                    <button type="button" className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none">
-                        Deseja apagar essa nota?
+                    <button type="button" className="w-full bg-slate-800 py-4 font-medium text-center text-sm text-slate-300 outline-none group">
+                        Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
                     </button>
                 </Dialog.Content>
             </Dialog.Portal>
