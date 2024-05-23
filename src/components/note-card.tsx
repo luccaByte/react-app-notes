638b8a0b6b81b7eb66a9d7c327d7e54d72d3f6a5
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-05-23 18:53:37 +0000

    fix: data das notas

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -16,7 +16,10 @@ export function NoteCard({note}: NoteCardProps) {
         <Dialog.Root>
             <Dialog.Trigger className='rounded-md bg-slate-800 p-5 text-left flex flex-col gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none'>
                 <span className='text-sm font-medium text-slate-300'>
-                    {note.date.toISOString()}
+                {formatDistanceToNow(note.date, {
+                    locale: ptBR,
+                    addSuffix: true,
+                })}
                 </span>
                 <p className='text-sm leading-6 text-slate-400'>
                     {note.content}
